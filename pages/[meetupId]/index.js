2df7c3614761609ae0c5c90f22b8b458cf0cadd5
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-17 15:39:48 +0530

    Adding custom components and CSS module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { Fragment } from "react";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Bananavarieties.jpg/1200px-Bananavarieties.jpg"
+      title="First meetup"
+      address="Dummy address"
+      desctiption="Dummy description"
+    />
+  );
+}
