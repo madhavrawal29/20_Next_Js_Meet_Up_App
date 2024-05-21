a311004043ce5d9bbff41c9091295ff0c4118795
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-21 15:30:02 +0530

    Exploring Server-side Rendering (SSR) with "getServerSideProps" - This will update the data with every incoming request.

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,24 @@ const DUMMY_MEETUPS = [
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
-export async function getStaticProps() {
-  // fetch data from an API
+
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 1,
   };
 }
+
+// export async function getStaticProps() {
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 1,
+//   };
+// }
