f325f2b20dbe70077735161e28dc29f06d847e6c
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-05-26 20:49:14 +0530

    After adding new meet up, redirecting user to the Home Page

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,11 @@
 // our-domain.com/new-meetup
 
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 export default function NewMeetupPage() {
+  const router = useRouter();
+
   async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch("/api/new-meetup", {
       method: "POST",
@@ -15,6 +18,8 @@ export default function NewMeetupPage() {
     const data = await response.json();
 
     console.log(data);
+
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
